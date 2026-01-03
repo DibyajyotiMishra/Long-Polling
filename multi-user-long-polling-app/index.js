f3ec8f4d963faa465fa50428527341a08c9b0f96
DibyajyotiMishra <dibyajyotimishra14@gmail.com>
2026-01-03 23:09:50 +0530

    feat: added redis for data persistence

--- multi-user-long-polling-app/index.js
+++ multi-user-long-polling-app/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const crypto = require("crypto");
+const { createClient } = require("redis");
 
 const app = express();
+const redis = createClient();
+redis.connect();
 
 app.use(express.json());
 app.use(express.static("public"));
+
 
 const users = {};
 
@@ -12,31 +16,30 @@ function getOrCreateUser(userId) {
     if (!users[userId]) {
         users[userId] = {
             userId,
-            notifications: [],
             clients: []
         }
     }
     return users[userId];
 }
 
-app.get("/poll/:userId", (req, res) => {
+
+app.get("/poll/:userId", async (req, res) => {
+    console.log(`${req.params.userId} just started polling...`);
     const user = getOrCreateUser(req.params.userId);
-    console.log(`${user.userId} just started polling...`);
 
     // checking for undelivered messages
-    const undeliveredMessage = user.notifications.find(message => !message.delivered);
+    const data = await redis.lRange(
+        `notifications:${user.userId}`,
+        0,
+        -1
+    );
+    const messages = data.map(JSON.parse);
+    const undeliveredMessage = messages.find(message => !message.delivered);
 
     // deliver them instantly
     if (undeliveredMessage) {
         console.log(`Delivering undelivered message for ${user.userId}`);
         return res.json(undeliveredMessage);
-    }
-
-    // if user has notifications, send them right away
-    if (user.notifications.length > 0) {
-        console.log(`${user.userId} has notifications. Sending them to the client right away!`);
-        // To close the connection immediately, we returnd the response.
-        return res.json(user.notifications.shift());
     }
 
     // if user has no notifications, wait for them
@@ -65,13 +68,23 @@ app.get("/poll/:userId", (req, res) => {
     }, 30000)
 })
 
-app.post("/ack/:userId", (req, res) => {
+app.post("/ack/:userId", async (req, res) => {
     const user = getOrCreateUser(req.params.userId);
     const { id } = req.body;
-    const message = user.notifications.find(message => message.id === id);
 
-    if (message) {
-        message.delivered = true;
+    const key = `notifications:${user.userId}`;
+    const items = await redis.lRange(key, 0, -1);
+
+    for (const item of items) {
+        const msg = JSON.parse(item);
+
+        // Replace message atomically
+        if (msg.id === id) {
+            msg.delivered = true;
+            await redis.lRem(key, 1, item);
+            await redis.rPush(key, JSON.stringify(msg));
+            break;
+        }
     }
 
     return res.json({
@@ -79,7 +92,7 @@ app.post("/ack/:userId", (req, res) => {
     })
 })
 
-app.post("/notify/:userId", (req, res) => {
+app.post("/notify/:userId", async (req, res) => {
     const user = getOrCreateUser(req.params.userId);
     const notification = {
         id: crypto.randomUUID(),
@@ -88,6 +101,12 @@ app.post("/notify/:userId", (req, res) => {
     };
 
     console.log(`New message for ${user.userId}`);
+
+    // Pushing the message to redis for persistence 
+    await redis.rPush(
+        `notifications:${user.userId}`,
+        JSON.stringify(notification)
+    );
 
     // If there are waiting clients, deliver the notification instantly
     if (user.clients.length > 0) {
@@ -100,11 +119,6 @@ app.post("/notify/:userId", (req, res) => {
         })
 
         user.clients = [];
-    }
-    // If there are no waiting clients, add the notification to the queue and send them once they are available
-    else {
-        console.log("No clients waiting for notifications. Adding to the queue...");
-        user.notifications.push(notification);
     }
 
     res.json({
